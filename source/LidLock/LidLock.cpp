07959b80037d0a3f43544fce7472d4482eee43e3
Thomas717 <thomaslewis@0xAA55.me>
2017-07-03 18:19:09 +0800

    Initial version - V1.0

--- source/LidLock/LidLock.cpp
+++ source/LidLock/LidLock.cpp
@@ -1,15 +1,15 @@
 #include <windows.h>
 
-LRESULT CALLBACK WndProc(HWND _v0, UINT _v1, WPARAM _v2, LPARAM _v3) {
-	if (_v1		== WM_POWERBROADCAST && _v2 == PBT_POWERSETTINGCHANGE) {
-			if (!memcmp(&((POWERBROADCAST_SETTING*)_v3)->PowerSetting, &GUID_LIDSWITCH_STATE_CHANGE, sizeof(GUID))) {
+LRESULT CALLBACK WndProc(HWND _v0, UINT _v1, WPARAM _v2, LPARAM _v3){
+	if (_v1 == WM_POWERBROADCAST && _v2 == PBT_POWERSETTINGCHANGE){
+			if (!memcmp(&((POWERBROADCAST_SETTING*)_v3)->PowerSetting, &GUID_LIDSWITCH_STATE_CHANGE, sizeof(GUID))){
 				if (!*((POWERBROADCAST_SETTING*)_v3)->Data) LockWorkStation();
 			}
 	}
 	return DefWindowProc(_v0, _v1, _v2, _v3);
 }
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd) {
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd){
 	HANDLE v0;
 	HWND v1;
 	MSG v2;
@@ -36,12 +36,12 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 		return -1;
 	}
 
-	if ((v1 = CreateWindow(v5, L"LidLock", 0, 0, 0, 0, 0, 0, 0, hInstance, 0)) == 0) {
+	if ((v1 = CreateWindow(v5, L"LidLock", 0, 0, 0, 0, 0, 0, 0, hInstance, 0)) == 0){
 		MessageBox(0, L"Failed to create a window.", L"Error", MB_OK | MB_ICONERROR);
 		return -1;
 	}
 
-	if ((v4 = RegisterPowerSettingNotification(v1, &GUID_LIDSWITCH_STATE_CHANGE, 0)) == 0) {
+	if ((v4 = RegisterPowerSettingNotification(v1, &GUID_LIDSWITCH_STATE_CHANGE, 0)) == 0){
 		MessageBox(0, L"Failed to register power change notification.", L"Error", MB_OK | MB_ICONERROR);
 		return -1;
 	}
